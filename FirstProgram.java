218a2da409b7ca987ec8bfe55f2efe25246567f0
ArthiK <arthi.kula@gmail.com>
2024-09-15 19:06:51 -0400

    Add files via upload
    Week1Day1

---
+++ FirstProgram.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	// main method - control space
+	public static void main(String[] args) {
+
+		int num=67;
+		boolean browerOpen=true;
+		float browserVersion=128.67f;
+		long mapValue=454654544;
+		char browerlog='C';
+		
+		String browserName="chrome";
+		
+		System.out.println("value getting from brower tab : "+num);
+		System.out.println("Print BrowerVersion :"+browserVersion);
+						
+		
+		// syso ---Control space
+		System.out.println("Welcome to Testleaf");
+
+		
+	}
+}

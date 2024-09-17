ed8c8b114a5dca7efbc3e596b54bc3d7616c4463
ArthiK <arthi.kula@gmail.com>
2024-09-17 13:31:38 -0400

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,22 @@
+package home.assignment.week1;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+	
+		int version =10;
+		
+		if (version>=0) {
+			System.out.println("Number 10 is positive");
+			
+			if (version<0) {
+				System.out.println("Number 10 is negative");
+			}
+				
+			}
+			
+		}
+
+	}
+
+
